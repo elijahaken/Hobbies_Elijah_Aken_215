2ea92591ba307a39a7b91f2672f33c693d2699d8
glacierlij <glacierlij@ELIJAHAKENEB95>
2024-02-09 20:53:57 -0700

    Add project files.

---
+++ Hobbbies2/wwwroot/js/calculator.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#calculate').click(function () {
+        // Validate input
+        var hours = $('#hours').val();
+        if (hours <= 0 || isNaN(hours)) {
+            alert('Please enter a positive number for hours.');
+            return;
+        }
+
+        // Perform calculation
+        var rate = $('#rate').val();
+        var total = hours * rate;
+        $('#total').val(total.toFixed(2)); // Set the total cost with two decimal places
+    });
+});
